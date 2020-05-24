f9aa0983050c093498d07ba93839c980010e06a2
Gideon Gurning <gideon.gurning@gmail.com>
2020-05-24 15:15:20 +0700

    Added dicee widget with functionality

--- lib/app/pages/Dicee.dart
+++ lib/app/pages/Dicee.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class Dicee extends StatelessWidget {
+class Dicee extends StatefulWidget {
+  @override
+  _DiceeState createState() => _DiceeState();
+}
+
+class _DiceeState extends State<Dicee> {
+  int leftDice = 1;
+  int rightDice = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Dicee'));
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('assets/images/dice-$leftDice.png'),
+              splashColor: Colors.grey,
+              onPressed: () => setState(() => leftDice = Random().nextInt(6) + 1),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('assets/images/dice-$rightDice.png'),
+              splashColor: Colors.grey,
+              onPressed: () => setState(() => rightDice = Random().nextInt(6) + 1),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
